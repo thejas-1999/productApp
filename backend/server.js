45a1814af987a016cc9bae946c16f6de835b49b3
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2024-11-20 11:32:02 +0530

    connect to the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
 
 const app = express();
 
@@ -7,5 +10,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(5000, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:5000`);
 });
